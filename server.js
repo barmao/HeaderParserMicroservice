02a8e150407404aff02e544ce84c01cf082632ce
barmao <bryanbarmao@gmail.com>
2022-01-31 21:31:49 +0300

    updated whoami logic

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+var ip = require('ip');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -21,8 +22,13 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  console.log(req.headers);
+  res.json({
+    ipaddress: ip.address(),
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
 });
 
 
