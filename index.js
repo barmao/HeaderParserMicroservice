aece9e15ae1abf48d01858b0161a5908999011eb
barmao <bryanbarmao@gmail.com>
2024-03-11 20:16:50 +0000

    Update IP request

--- index.js
+++ index.js
@@ -20,12 +20,15 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get("/api/whoami", function (req, res) {
-  console.log(req.headers);
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
   res.json({
-    ipaddress: ip.address(),
-    language: req.headers['accept-language'],
-    software: req.headers['user-agent']
+      ipaddress,
+      language,
+      software
   });
 });
 
